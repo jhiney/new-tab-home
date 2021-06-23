d59859c24084200a638f1b08936404fae9b0dcb7
Jake Hiney <hiney.jake@outlook.com>
2021-06-23 11:06:43 -0500

    feat: new tab redirects to feedbin

--- background.js
+++ background.js
@@ -1,7 +1,10 @@
-let color = '#3aa757';
+let color = "#3aa757";
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+	chrome.storage.sync.set({ color });
+	console.log("Default background color set to %cgreen", `color: ${color}`);
 });
 
+chrome.tabs.onCreated.addListener(() => {
+	chrome.tabs.update({ url: "https://feedbin.com" });
+});
