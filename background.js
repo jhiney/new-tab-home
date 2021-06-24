870f343e104d870cd53c18e6cbdb7584cdb7fba8
Jake Hiney <hiney.jake@outlook.com>
2021-06-24 07:56:20 -0500

    docs: remove console logs

--- background.js
+++ background.js
@@ -2,13 +2,12 @@ let color = "#3aa757";
 
 chrome.runtime.onInstalled.addListener(() => {
 	chrome.storage.sync.set({ color });
-	console.log("Default background color set to %cgreen", `color: ${color}`);
 });
 
 chrome.tabs.onCreated.addListener(() => {
 	chrome.tabs.query({ active: true }, (tabs) => {
 		let url = tabs[0].url;
-		console.log(url);
+
 		if (url === "") {
 			chrome.tabs.update({ url: "https://feedbin.com" });
 		}
